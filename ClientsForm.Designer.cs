b3872a143890356f5ee786131277ab30b0b44eba
Arvi <artemtolstykn@gmail.com>
2026-02-14 19:42:37 +0300

    Убрал уконки во всех формах, кроме основной

--- ClientsForm.Designer.cs
+++ ClientsForm.Designer.cs
@@ -95,6 +95,7 @@ namespace LibraryAppForProject
             this.Controls.Add(this.dataGridView1);
             this.MinimumSize = new System.Drawing.Size(602, 378);
             this.Name = "ClientsForm";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Клиенты";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
